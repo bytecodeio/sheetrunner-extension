0defe18a3169963b5c3f02b984cd7887da0d385b
Colin Roy-Ehri <colin.roy.ehri@gmail.com>
2021-11-10 19:08:22 -0700

    Looker Components!

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,104 +1,80 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { HomeProps } from './types'
 import { ContentContainer } from '../../styles';
 import { Upload } from '../../utils/upload';
 import { GenerateSQL } from '../../utils/generate_sql';
 import { ExecuteSQL } from '../../utils/execute_sql';
-import Dropdown from '../../../node_modules/react-bootstrap/Dropdown';
-import '../../../node_modules/bootstrap/dist/css/bootstrap.css';
-import '../../../node_modules/stylesheet.css';
+import { Select, Space, SpaceVertical, Button } from '@looker/components'
+import { IDBConnection } from '@looker/sdk';
 
 
 
 
-export const Home: React.FC<HomeProps> = async ({sdk}) => {
+export const Home: React.FC<HomeProps> = ({ sdk }) => {
 
   let [inputfile, updatefile] = useState("")
   let [sql, updateSQL] = useState("")
   let [result, updateResult] = useState("")
   let [connection_name, setConnectionName] = useState("bytecode_looker_bigquery")
+  let [all_connections, setAllConnections] = useState<IDBConnection[]>([])
+  let [showNav, setShowNav] = useState<boolean>(true)
 
-  const connection_response = await sdk.ok(sdk.all_connections())
+  // This will trigger once, at startup
+  useEffect(() => {
+    fetchConnections()
+  }, [])
 
-  console.log(connection_response)
+  // This will get the connections and set it as a variable
+  const fetchConnections = async () => {
+    const response = await sdk.ok(sdk.all_connections())
+    setAllConnections(response)
+  }
+
+  // This function toggles the nav
+  const toggleNav = () => setShowNav(!showNav)
+
+  console.log('response: ' + all_connections)
 
 
   return (
-        <ContentContainer flexDirection='row' justifyContent='space-between'>
+    <Space>
+      {showNav ?
+      <div className="wrapper">
+        <SpaceVertical id="sidebar">
+          <label htmlFor="model">Choose a connection:</label>
+          <Select
+            id='selectConnection'
+            onChange={(e)=>console.log(e)}
+            options={all_connections ? all_connections.map(c => { return { value: c.name } }) : [{ value: 'loading...' }]}
+          />
+          <Button type="button" id="sidebarCollapse" className="btn btn-info" onClick={toggleNav}>Hide</Button>
+        </SpaceVertical>
+      </div>
+      : 
+      <Button onClick={toggleNav}>Show</Button>
+      }
 
 
+      <img src="https://storage.googleapis.com/bytecode-hackathon-2021/SheetRunner_final.png" height="150px" />
 
-        <div class="wrapper">
+      <div id="buttons" >
 
-        <nav id="sidebar">
-        <div class="sidebar-header">
-            <h3>Connection</h3>
+        <input type="file" id="fileUpload" />
+        <input type="button" id="upload" value="Upload" onClick={(e) => Upload(updatefile)} />
+        <input type="button" id="parse" value="Generate SQL" onClick={(e) => GenerateSQL(inputfile, updateSQL, sdk, connection_name)} />
+        <input type="button" id="upload" value="Execute SQL" onClick={(e) => ExecuteSQL(sql, updateResult, sdk, connection_name)} />
 
-            <div class="model-selection">
-            <label for="model">Choose a model:</label>
-
-            <Dropdown id="uniquename">
-              <Dropdown.Toggle variant="success" id="dropdown-basic">
-                Dropdown Button
-              </Dropdown.Toggle>
-
-              <Dropdown.Menu id="uniquename2">
-                {connection_response.map((connection) => {
-                  return <Dropdown.Item
-                        href="#/action-1">
-                        {connection}
-                      </Dropdown.Item>})
-                }
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-
-            </div>
-        </div>
-
-       </nav>
-
-
-
-    <div id="content">
-
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-
-                <button type="button" id="sidebarCollapse" class="btn btn-info">
-                    <i class="fas fa-align-left"></i>
-                    <span>Toggle Sidebar</span>
-                </button>
-            </div>
-        </nav>
-    </div>
-</div>
-
-
-
-
-
-            <img src="https://storage.googleapis.com/bytecode-hackathon-2021/SheetRunner_final.png" height="150px"/>
-
-            <div id="buttons" >
-
-              <input type="file" id="fileUpload" />
-              <input type="button" id="upload" value="Upload" onClick={(e) => Upload(updatefile)} />
-              <input type="button" id="parse" value="Generate SQL" onClick={(e) => GenerateSQL(inputfile, updateSQL, sdk, connection_name)} />
-              <input type="button" id="upload" value="Execute SQL" onClick={(e) => ExecuteSQL(sql, updateResult, sdk, connection_name)} />
-
-            </div>
-            <hr />
-            <div id="dvCSV">
-            </div>
-            <hr />
-            <div id="sql">
-              {sql}
-            </div>
-            <div id="results">
-              {result}
-            </div>
-        </ContentContainer>
+      </div>
+      <hr />
+      <div id="dvCSV">
+      </div>
+      <hr />
+      <div id="sql">
+        {sql}
+      </div>
+      <div id="results">
+        {result}
+      </div>
+    </Space>
   )
 };
